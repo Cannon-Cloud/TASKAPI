4f2d4c0a37ec887a9f7395992b3c9a696beaed70
Cannon-Cloud <clarence@cannon.cloud>
2021-07-30 13:02:47 -0700

    Auth

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,17 +1,33 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './auth.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-
+import * as bcrypt from 'bcrypt';
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialDto;
 
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        // duplicate user
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
